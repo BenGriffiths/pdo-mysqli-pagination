f553d98333441171d7c8c883c7294c0304fe6b8c
Steven <stevenm.wills@gmail.com>
2013-08-08 11:27:10 -0500

    Update example_pdo.php
    Added exception handling, removed die() to ensure all html / xml is output regardless of success

--- example/example_pdo.php
+++ example/example_pdo.php
@@ -25,58 +25,58 @@
 
 
 /*
- * Show all errors (not required of course)
- */
+* Show all errors (not required of course)
+*/
 ini_set('display_errors','On');
 error_reporting(-1);
 
 
 /*
- * Include the pagination.php class file
- */
+* Include the pagination.php class file
+*/
 require_once('../src/pagination.php');
 
 
 /*
- * Connect to the database (Replacing the XXXXXX's with the correct details)
- */
+* Connect to the database (Replacing the XXXXXX's with the correct details)
+*/
 try
 {
-    $dbh = new PDO('mysql:host=XXXXXX;dbname=XXXXXX', 'XXXXXX', 'XXXXXX');
+    $dbh = new PDO('mysql:host=localhost;dbname=jokes', 'stevenmw', '5beb811');
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e)
 {
     print "Error!: " . $e->getMessage() . "<br/>";
-    die();
 }
 
 
 /*
- * Get and/or set the page number we are on
- */
+* Get and/or set the page number we are on
+*/
 if(isset($_GET['page']))
 {
-	$page = $_GET['page'];
+$page = $_GET['page'];
 }
 else
 {
-	$page = 1;
+$page = 1;
 }
 
 
 /*
- * Set a few of the basic options for the class, replacing the URL with your own of course
- */
+* Set a few of the basic options for the class, replacing the URL with your own of course
+*/
 $options = array(
-    'results_per_page'              => 6,
-    'url'                           => 'http://www.mysite.com/example_pdo.php?page=*VAR*',
-    'db_handle'                     => $dbh
+    'results_per_page' => 6,
+    'url' => 'http://www.mysite.com/example_pdo.php?page=*VAR*',
+    'db_handle' => $dbh
 );
 
 
 /*
- * Create the pagination object
- */
+* Create the pagination object
+*/
 try
 {
     $paginate = new pagination($page, 'SELECT * FROM demo_table ORDER BY id', $options);
@@ -89,36 +89,36 @@ catch(paginationException $e)
 
 
 /*
- * If we get a success, carry on
- */
+* If we get a success, carry on
+*/
 if($paginate->success == true)
 {
 
     /*
-     * Fetch our results
-     */
+* Fetch our results
+*/
     $result = $paginate->resultset->fetchAll();
 
     /*
-     * Echo out the UL with the page links
-     */
+* Echo out the UL with the page links
+*/
     echo '<p>'.$paginate->links_html.'</p>';
 
     /*
-     * Echo out the total number of results
-     */
+* Echo out the total number of results
+*/
     echo '<p style="clear: left; padding-top: 10px;">Total Results: '.$paginate->total_results.'</p>';
 
     /*
-     * Echo out the total number of pages
-     */
+* Echo out the total number of pages
+*/
     echo '<p>Total Pages: '.$paginate->total_pages.'</p>';
 
     echo '<p style="clear: left; padding-top: 10px; padding-bottom: 10px;">-----------------------------------</p>';
 
     /*
-     * Work with our data rows
-     */
+* Work with our data rows
+*/
     foreach($result as $row)
     {
         echo '<p>'.$row['demo_field'].', '.$row['demo_field_two'].'</p>';
